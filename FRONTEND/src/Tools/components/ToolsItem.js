511b9c7bfc6231a11421202133e8639e06770905
Laith Adi <Laith_adi@hotmail.com>
2020-03-29 18:00:16 -0400

    FINAL COMMIT

--- FRONTEND/src/Tools/components/ToolsItem.js
+++ FRONTEND/src/Tools/components/ToolsItem.js
@@ -1,10 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "./ToolItem.css";
 
 const ToolsItem = props => {
+  const [checkin, setCheckIn] = useState();
+  const [checkout, setCheckout] = useState();
 
+  const addTool = async event => {
+    try {
+      await fetch("http://localhost:2000/mytools/add", {
+        method: "POST",
+        body: JSON.stringify({
+          name: props.name,
+          description: props.description,
+          price: props.price,
+          availability: props.availability,
+          image: props.image
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+      setCheckIn(false);
+      setCheckout(true);
+    } catch (err) {}
+  };
+
+  const removeTool = async event => {
+    try {
+      const response = await fetch("http://localhost:2000/mytools/remove", {
+        method: "DELETE",
+        body: JSON.stringify({
+          name: props.name,
+          description: props.description,
+          price: props.price,
+          availability: props.availability,
+          image: props.image
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+      setCheckIn(true);
+      setCheckout(false);
+    } catch (err) {}
+  };
 
   return (
     <li className="tool-item">
@@ -16,18 +53,19 @@ const ToolsItem = props => {
           <div className="b">
             <h2>{props.name}</h2>
             <p>
-              {props.description}  {' '}Availability:{" "}
-              {props.availability}
+              {props.description} Availability: {props.availability}
             </p>
             <p>{props.price}</p>
           </div>
         </div>
       </Link>
       <div className="c">
-        <button onClick={} disabled={checkout}>CHECK-OUT</button>
+        <button onClick={addTool} disabled={checkout}>
+          CHECK-OUT
+        </button>
       </div>
-      <div className="d" disabled={checkin}>
-        <button onClick={}>CHECK-IN</button>
+      <div className="d">
+        <button onClick={removeTool} disabled={checkin}>CHECK-IN</button>
       </div>
     </li>
   );
--- FRONTEND/src/Tools/pages/Tool.js
+++ FRONTEND/src/Tools/pages/Tool.js
@@ -3,9 +3,29 @@ import { useParams } from "react-router-dom";
 import OneToolItem from "../components/OneToolItem";
 
 const Tool = props => {
+  const [tool, setTool] = useState();
+  const toolname = useParams().toolname;
+
+  useEffect(() => {
+    const fetchTools = async () => {
+      try {
+        const url = `http://localhost:2000/tool/${toolname}`;
+        const response = await fetch(url);
+        console.log("kfakj");
+        const data = response.json();
+        console.log("kfakj");
+        setTool(data.name);
+        console.log(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    fetchTools();
+  }, [toolname]);
+
   return (
     <main>
-
+      <h1>{toolname}</h1>
     </main>
   );
 };
